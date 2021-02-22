40bbafdffd6aad789727088072978f11a8f61424
Isael03 <michael.puelle.ordenes@gmail.com>
2021-02-22 20:25:21 -0300

    gitignore

--- varios/variables_entorno/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express') 
-const app = express()
-require('dotenv').config({path:'./.env'})
-
-const puerto = process.env.PORT || 3000;
-app.listen(puerto, ()=>{
-    console.log('Servidor funcionando en puerto '+puerto);
-})
